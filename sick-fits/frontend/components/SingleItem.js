b6f98962e1964a4f92b58728e6876ec157bd8f3a
Valentin Ageev <sazuru@gmail.com>
2020-04-19 17:04:38 +0300

    Added Pagination

--- sick-fits/frontend/components/SingleItem.js
+++ sick-fits/frontend/components/SingleItem.js
@@ -6,22 +6,22 @@ import Head from 'next/head';
 import Error from './ErrorMessage';
 
 const SingleItemStyles = styled.div`
-max-width: 1200px;
-margin: 2rem auto;
-box-shadow: ${(props) => props.theme.bs}
-display: grid;
-grid-auto-columns: 1fr;
-grid-auto-flow: column;
-min-height: 800px;
-img {
-  width: 100%;
-  height: 100%;
-  object-fit: contain;
-}
-.details {
-  margin: 3rem;
-  font-size: 2rem;
-}
+  max-width: 1200px;
+  margin: 2rem auto;
+  box-shadow: ${(props) => props.theme.bs};
+  display: grid;
+  grid-auto-columns: 1fr;
+  grid-auto-flow: column;
+  min-height: 800px;
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+  }
+  .details {
+    margin: 3rem;
+    font-size: 2rem;
+  }
 `;
 
 const SINGLE_ITEM_QUERY = gql`
