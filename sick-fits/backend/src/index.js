35c8b8f20ada635f43909f19e482d4fb10fa94dc
Valentin Ageev <sazuru@gmail.com>
2020-05-11 14:57:12 +0300

    Started to adding permissons

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -8,25 +8,39 @@ const db = require('./db');
 const server = createServer();
 
 server.express.use(cookieParser());
-// Decode JWT token
+
+// decode the JWT so we can get the user Id on each request
 server.express.use((req, res, next) => {
   const { token } = req.cookies;
   if (token) {
     const { userId } = jwt.verify(token, process.env.APP_SECRET);
+    // put the userId onto the req for future requests to access
     req.userId = userId;
   }
   next();
 });
-// TODO Use express middlware to populate current user
+
+// 2. Create a middleware that populates the user on each request
+
+server.express.use(async (req, res, next) => {
+  // if they aren't logged in, skip this
+  if (!req.userId) return next();
+  const user = await db.query.user(
+    { where: { id: req.userId } },
+    '{ id, permissions, email, name }'
+  );
+  req.user = user;
+  next();
+});
 
 server.start(
   {
     cors: {
       credentials: true,
-      origin: process.env.FRONTEND_URL
-    }
+      origin: process.env.FRONTEND_URL,
+    },
   },
-  deets => {
+  (deets) => {
     console.log(`Server is now running on port http://localhost:${deets.port}`);
   }
 );
